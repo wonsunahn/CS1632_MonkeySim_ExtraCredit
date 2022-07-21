d63b251fa73e9f91026f6c12f53d9b24cc9c5741
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2022-07-20 20:32:18 -0400

    Updates for Maven

--- run.sh
+++
@@ -1,5 +0,0 @@
-mkdir bin
-
-javac -d bin -cp "CommandLineJunit/*:quickcheck-jars/*" src/*.java
-
-java -Xverify:none -cp "bin:CommandLineJunit/*:quickcheck-jars/*" MonkeySim $1
--- runTest.sh
+++
@@ -1,5 +0,0 @@
-mkdir bin
-
-javac -d bin -cp "CommandLineJunit/*:quickcheck-jars/*" src/*.java
-
-java -cp "CommandLineJunit/*:quickcheck-jars/*:bin" TestRunner
